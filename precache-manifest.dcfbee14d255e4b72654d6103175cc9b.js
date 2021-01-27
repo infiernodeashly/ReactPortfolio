cdbb46cf8e88ea44349d369d3ab6648aeb5e858f
Ashly Mayle <infiernodeashly@gmail.com>
2021-01-27 14:28:25 -0500

    Updates

---
+++ precache-manifest.dcfbee14d255e4b72654d6103175cc9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7b81b2a2c5452affb3e169069164b0",
+    "url": "/ReactPorfolio/index.html"
+  },
+  {
+    "revision": "eeccbae8503df3ff8702",
+    "url": "/ReactPorfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "44f9fb2ad6c07f25039f",
+    "url": "/ReactPorfolio/static/js/2.0962ac68.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/ReactPorfolio/static/js/2.0962ac68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeccbae8503df3ff8702",
+    "url": "/ReactPorfolio/static/js/main.5c17c3d4.chunk.js"
+  },
+  {
+    "revision": "f5842d8288c271f19bff",
+    "url": "/ReactPorfolio/static/js/runtime-main.676f2a46.js"
+  }
+]);
